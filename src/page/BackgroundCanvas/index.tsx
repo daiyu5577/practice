64e16937f481db1230ba46d16d4d24eaccb8a2c4
daiyu <dai.yu@olaola.chat>
2024-06-15 12:21:30 +0800

    feat: 调整canvas

--- src/page/BackgroundCanvas/index.tsx
+++ src/page/BackgroundCanvas/index.tsx
@@ -15,10 +15,13 @@ class CanvasDraw {
   graphStackArr: {
     x: number
     y: number
+    targetX: number
+    targetY: number
     img: HTMLImageElement
   }[]
 
   maxStack = 30
+  timer = 0
 
   constructor(canvasDOM: HTMLCanvasElement, sources: string[]) {
     this.canvasDOM = canvasDOM
@@ -31,14 +34,20 @@ class CanvasDraw {
 
   // 初始化
   init() {
+    this.bindFn()
     this.loadSources()
+  }
+
+  // 绑定
+  bindFn() {
+    this.run = this.run.bind(this)
   }
 
   // 加载资源
   loadSources() {
     this.sources.forEach(v => {
       const img = new Image()
-      img.onload = (e) => {
+      img.onload = () => {
         this.imageDOMArr.push(img)
         if (this.imageDOMArr.length >= this.sources.length) {
           this.run()
@@ -53,59 +62,64 @@ class CanvasDraw {
     const curImgIndex = Math.floor(Math.random() * (this.imageDOMArr.length) + 1)
     return {
       x: Math.floor(Math.random() * this.canvasDOM.width),
-      y: Math.floor(Math.random() * this.canvasDOM.height),
-      img: this.imageDOMArr[curImgIndex]
+      y: Math.floor(this.canvasDOM.height + Math.random() * 20),
+      targetX: Math.floor(Math.random() * this.canvasDOM.width),
+      targetY: Math.floor(Math.random() * this.canvasDOM.height),
+      img: this.imageDOMArr[curImgIndex - 1]
+    }
+  }
+
+  // 检测绘制完成的图
+  checkGraphStackArr() {
+    for (let i = this.graphStackArr.length - 1; i >= 0; i--) {
+      const graphItem = this.graphStackArr[i];
+      if (graphItem.y <= graphItem.targetY) {
+        this.graphStackArr.splice(i, 1)
+      }
+    }
+    if (!(this.graphStackArr.length >= this.maxStack)) {
+      this.graphStackArr.push(this.creatGraph())
     }
   }
 
   // 绘制
-  draw() { }
+  draw() {
+    this.ctx2d.clearRect(0, 0, this.canvasDOM.width, this.canvasDOM.height)
+    this.graphStackArr.forEach((v) => {
+      this.ctx2d.beginPath()
+      v.x < v.targetX ? v.x += 1 : v.x -= 1
+      v.y > v.targetY && (v.y -= 1)
+      this.ctx2d.drawImage(v.img, v.x, v.y, v.img.width, v.img.height)
+      // this.ctx2d.fillStyle = '#000'
+      // this.ctx2d.fillRect(v.x, v.y, 10, 10)
+    })
+  }
 
   // 运行
   run() {
-    setInterval(() => {
-      if (!(this.graphStackArr.length >= this.maxStack)) {
-        this.graphStackArr.push(this.creatGraph())
-      }
-    }, 1000)
+    this.timer = setInterval(() => {
+      this.checkGraphStackArr()
+      this.draw()
+    }, 1000 / 40)
+    // requestAnimationFrame(this.run)
   }
 
+  // 销毁
+  destroy() {
+    clearInterval(this.timer)
+  }
 }
 
 export default function BackgroundCanvas() {
 
   const winScreen = useRef(window.screen)
   const canvasDOM = useRef<HTMLCanvasElement>(null)
-  const ctx2d = useRef<CanvasRenderingContext2D>()
-
-  // 图片资源
-  const imageDOMArr = useRef<HTMLImageElement[]>([])
-
-  // 加载资源
-  const init = (sources: string[]) => {
-    sources.forEach(v => {
-      const img = new Image()
-      img.onload = (e) => {
-        imageDOMArr.current.push(img)
-        if (imageDOMArr.current.length >= sources.length) run()
-      }
-      img.src = v
-    })
-  }
-
-  // 运行
-  const run = () => {
-    const ctx = canvasDOM.current?.getContext('2d')
-  }
-
-  // 创建图形对象
-  const creatImgObj = () => {
-
-  }
+  const canvasDraw = useRef<CanvasDraw>()
 
   useEffect(() => {
-    ctx2d.current = canvasDOM.current!.getContext('2d') as CanvasRenderingContext2D
-    init(staticImages)
+    const instance = new CanvasDraw(canvasDOM.current as HTMLCanvasElement, staticImages)
+    canvasDraw.current = instance
+    return () => instance.destroy()
   }, [])
 
   return (
