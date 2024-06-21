742d7823e0c9d795f89874c9c6eb523e6ce8dff8
daiyu <dai.yu@olaola.chat>
2024-06-21 18:26:49 +0800

    feat: 球坐标计算

---
+++ src/page/Preserve3d/components/RotateBallXY/index.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import styles from './index.module.less'
+
+const getBallPostion = (deg: number, radius: number, needNum: number) => {
+  const getRad = (deg: number) => deg * Math.PI / 180 // 角度转弧度
+  const averageAngle = 360 / needNum // 平均角度
+
+  // 若转换坐标系 z 轴向上、则该点为 z 坐标
+  // 不转换坐标系 z 轴垂直屏幕，实现球面坐标、则该点为 y 坐标
+  const vRad = getRad(deg) // 垂直弧度
+  const y = Math.floor(radius * Math.sin(vRad))
+
+  const ShadowHypotenuse = radius * Math.cos(vRad) // 平面投影斜边
+
+  const postionArr = []
+
+  for (let i = 0; i < needNum; i++) {
+    const x = Math.floor(ShadowHypotenuse * Math.cos(getRad(i * averageAngle)))
+    const z = Math.floor(ShadowHypotenuse * Math.sin(getRad(i * averageAngle)))
+    postionArr.push({ x, y, z })
+  }
+  return postionArr
+}
+
+const layer_1 = getBallPostion(-45, 60, 8)
+const layer_2 = getBallPostion(0, 120, 16)
+const layer_3 = getBallPostion(45, 100, 12)
+
+export default function RotateBallXY() {
+  return (
+    <div className={styles.rotateZ}>
+      <div className="box">
+        <div className="boxCenter">
+          {
+            layer_1.map((v, i, t) => (
+              <div
+                key={i}
+                className="item"
+                style={{
+                  transform: `translate3d(${v.x}px, ${v.y}px, ${v.z}px)`
+                }}
+              ></div>
+            ))
+          }
+          {
+            layer_2.map((v, i, t) => (
+              <div
+                key={i}
+                className="item"
+                style={{
+                  transform: `translate3d(${v.x}px, ${v.y}px, ${v.z}px)`
+                }}
+              ></div>
+            ))
+          }
+          {
+            layer_3.map((v, i, t) => (
+              <div
+                key={i}
+                className="item"
+                style={{
+                  transform: `translate3d(${v.x}px, ${v.y}px, ${v.z}px)`
+                }}
+              ></div>
+            ))
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/page/Preserve3d/index.tsx
+++ src/page/Preserve3d/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import RotateZ from './components/RotateZ' // 分割圆、Y轴旋转
 import RotateBallTranslateZ from './components/RotateBallTranslateZ' // ball
+import RotateBallXY from './components/RotateBallXY' // ball
 import styles from './index.module.less'
 
 export default function Preserve3d() {
   return (
     <div className={styles.preserve3d}>
       {/* <RotateZ /> */}
-      <RotateBallTranslateZ />
+      {/* <RotateBallTranslateZ /> */}
+      <RotateBallXY />
     </div>
   )
 }
